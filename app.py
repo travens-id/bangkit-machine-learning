175ee448065708c3fd8765fb7f096a8b92b9207b
algonacci <algonacci1@gmail.com>
2022-05-20 22:45:16 +0700

    feat: Make upload image endpoint
    - prepare the /predict enpoint
    - save the inputted image from user in the uploads folder

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, jsonify, request
+import os
+from flask import Flask, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from PIL import Image
+
 
 app = Flask(__name__)
+
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER'] = './uploads/'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
 def index():
@@ -13,13 +24,22 @@ def index():
 
 @app.route('/post', methods=['POST'])
 def post():
-    if request.method == 'POST':
-        data = request.get_json()
-        return jsonify(data)
+    data = request.get_json()
+    return jsonify(data)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # return redirect(url_for('download_file', name=filename))
+        return "Uploaded!"
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({'message': 'Not found', 'status_code': 404})
+    return jsonify({'message': 'Endpoint not found', 'status_code': 404})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
