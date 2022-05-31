ff58e2a12e971062f6f617ac288c268d29136a2c
algonacci <algonacci1@gmail.com>
2022-05-31 21:44:51 +0700

    refactoring: formatting and prepare for production

--- app.py
+++ app.py
@@ -18,20 +18,24 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['DOWNLOAD_FOLDER'] = 'downloads/'
 
 LABEL_FILENAME = 'labels/label_map.pbtxt'
-category_index = label_map_util.create_category_index_from_labelmap(LABEL_FILENAME, use_display_name=True)
+category_index = label_map_util.create_category_index_from_labelmap(
+    LABEL_FILENAME, use_display_name=True)
 
 print('------ Loading model ------')
 model = 'model/'
 hub_model = hub.load(model)
 print('------ Model Loaded ------')
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
+
 def load_image_into_numpy_array(image):
-  (image_width, image_height) = image.size
-  return np.array(image.getdata()).reshape((1, image_height, image_width, 3)).astype(np.uint8)
+    (image_width, image_height) = image.size
+    return np.array(image.getdata()).reshape((1, image_height, image_width, 3)).astype(np.uint8)
+
 
 @app.route('/')
 def index():
@@ -42,10 +46,12 @@ def index():
     }
     return jsonify(hello_json)
 
+
 @app.route('/post', methods=['POST'])
 def post():
     data = request.get_json()
     return jsonify(data)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -56,15 +62,15 @@ def predict():
         image_path = Image.open(file)
         image_path = image_path.convert('RGB')
         image_np = load_image_into_numpy_array(image_path)
-        flip_image_horizontally = False 
+        flip_image_horizontally = False
         convert_image_to_grayscale = False
         if(flip_image_horizontally):
             image_np[0] = np.fliplr(image_np[0]).copy()
         if(convert_image_to_grayscale):
             image_np[0] = np.tile(
-            np.mean(image_np[0], 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+                np.mean(image_np[0], 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
         results = hub_model(image_np)
-        result = {key:value.numpy() for key,value in results.items()}
+        result = {key: value.numpy() for key, value in results.items()}
         label_id_offset = 0
         image_np_with_detections = load_image_into_numpy_array(image_path)
         viz_utils.visualize_boxes_and_labels_on_image_array(
@@ -89,7 +95,8 @@ def predict():
             csvfile.close()
             label = 'Tidak Terdeteksi'
         finally:
-            predicted_image = Image.fromarray(image_np_with_detections.squeeze())
+            predicted_image = Image.fromarray(
+                image_np_with_detections.squeeze())
             predicted_image.save('downloads/' + filename)
             json = {
                 "label": label,
@@ -97,13 +104,18 @@ def predict():
             }
             return jsonify(json)
 
+
 @app.route('/downloads/<name>')
 def download_file(name):
     return send_from_directory(app.config["DOWNLOAD_FOLDER"], name)
+
 
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({'message': 'Endpoint not found', 'status_code': 404})
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', ssl_context=('/etc/letsencrypt/live/travens-api.my.id/cert.pem',
+                                                      '/etc/letsencrypt/live/travens-api.my.id/privkey.pem'))
+    # app.run(debug=False, host='0.0.0.0')
