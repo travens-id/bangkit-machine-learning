6f37c8cd8fb10e2ae8549d8b6be95f99f1c922df
algonacci <algonacci1@gmail.com>
2022-06-01 11:08:30 +0700

    fix: Update for deployment

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def predict():
             predicted_image.save('downloads/' + filename)
             json = {
                 "label": label.replace('_', ' '),
-                "image_url": 'http://travens-api.my.id/downloads/' + filename
+                "image_url": 'https://travens-api.my.id/downloads/' + filename
             }
             return jsonify(json)
 
@@ -116,8 +116,5 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='34.101.216.207', port=443, ssl_context=('/etc/letsencrypt/live/travens-api.my.id/cert.pem',
+    app.run(debug=False, host='0.0.0.0', port=443, ssl_context=('/etc/letsencrypt/live/travens-api.my.id/cert.pem',
                                                                 '/etc/letsencrypt/live/travens-api.my.id/privkey.pem'))
-    # app.run(debug=False, host='0.0.0.0', port=443, ssl_context=('/etc/letsencrypt/live/travens-api.my.id/cert.pem',
-    #                                                             '/etc/letsencrypt/live/travens-api.my.id/privkey.pem'))
-    # app.run(debug=False, host='0.0.0.0')
